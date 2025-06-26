9f67b7eb35fbdea13799ee430f36531b476eecb6
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2025-06-26 21:03:50 +0300

    Auth

--- user-service/src/main/java/com/taskflow/user_service/service/AuthService.java
+++ user-service/src/main/java/com/taskflow/user_service/service/AuthService.java
@@ -3,9 +3,12 @@ package com.taskflow.user_service.service;
 import com.taskflow.user_service.dto.AuthResponse;
 import com.taskflow.user_service.dto.LoginRequest;
 import com.taskflow.user_service.dto.RegisterRequest;
+import com.taskflow.user_service.dto.UserResponse;
 import org.springframework.security.core.Authentication;
 
 public interface AuthService {
     AuthResponse register(RegisterRequest request);
     AuthResponse login(LoginRequest request);
+    UserResponse getCurrentUser(Authentication authentication);
+    void logout(Authentication authentication);
 }
