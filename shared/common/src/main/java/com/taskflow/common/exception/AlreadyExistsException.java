ab1cb19b1696cf3f64804df69f33a8cd720e31a9
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2025-07-15 05:33:02 +0300

    Pom update

--- shared/common/src/main/java/com/taskflow/common/exception/AlreadyExistsException.java
+++ shared/common/src/main/java/com/taskflow/common/exception/AlreadyExistsException.java
@@ -2,12 +2,15 @@ package com.taskflow.common.exception;
 
 import lombok.Getter;
 
-@Getter
 public class AlreadyExistsException extends RuntimeException {
     private final String entityName;
 
     public AlreadyExistsException(String message, String entityName) {
         super(message);
         this.entityName = entityName;
+    }
+
+    public String getEntityName() {
+        return entityName;
     }
 }
--- shared/common/src/main/java/com/taskflow/common/exception/NotFoundException.java
+++ shared/common/src/main/java/com/taskflow/common/exception/NotFoundException.java
@@ -1,13 +1,14 @@
 package com.taskflow.common.exception;
 
-import lombok.Getter;
-
-@Getter
 public class NotFoundException extends RuntimeException {
     private final String entityName;
 
     public NotFoundException(String message, String entityName) {
         super(message);
         this.entityName = entityName;
+    }
+
+    public String getEntityName() {
+        return entityName;
     }
 }
